c93107461f0fe05c15a663d40dcc1491f14f5491
jayson7 <lexxiijoo70@gmail.com>
2021-10-30 00:37:43 +0100

    jarvis

--- demo_jarvis1/main.py
+++ demo_jarvis1/main.py
@@ -1,6 +1,18 @@
 import pyttsx3 
 import jarvis
-
+import datetime
 engine = pyttsx3.init()
-engine.say("Hello Jayson, sorry i've been giving you issues all day long, let's get started")
-engine.runAndWait()
+
+def speak(audio):
+
+    engine.say(audio)
+    engine.runAndWait()
+
+# speak("Hello jayson")
+
+
+def timer():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak(Time)
+
+timer()
