3ad9b726950767890addce6be6d920b46129219b
jayson7 <lexxiijoo70@gmail.com>
2021-11-07 14:34:07 +0100

    jarvis version 3

--- demo_v3/main.py
+++ demo_v3/main.py
@@ -73,3 +73,13 @@ if __name__ == "__main__":
             print(results)
             speak(results)
  
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+ 
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+ 
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+ 
+ 
