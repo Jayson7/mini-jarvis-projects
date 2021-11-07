2e6dd6b7329225e5c5f94d411921ab43a5debe18
jayson7 <lexxiijoo70@gmail.com>
2021-11-07 14:34:33 +0100

    jarvis version 3

--- demo_v3/main.py
+++ demo_v3/main.py
@@ -93,3 +93,7 @@ if __name__ == "__main__":
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
  
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+ 
