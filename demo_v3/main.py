38d8b574ad0b6316848c9290b20fe3d184a9c66d
jayson7 <lexxiijoo70@gmail.com>
2021-11-07 14:32:52 +0100

    jarvis version 3

--- demo_v3/main.py
+++ demo_v3/main.py
@@ -6,3 +6,14 @@ import webbrowser
 import os
 import smtplib
  
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+ 
+ 
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+ 
+ 
